fd097f27d6b8887c269f2ae56ba1761ca815352b
tri4time1 <tri4time@gmail.com>
2026-08-21 11:20:15 -0500

    Add a preview article template

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import EpisodePage from "../app/page";
 import Homepage from "../app/homepage";
+import ArticleTemplate from "../app/article";
 import "../app/globals.css";
 import "../src/tracing-path-ribbon.js";
 
 const isEpisodeRoute = window.location.pathname.endsWith("/episode.html");
+const isArticleRoute = window.location.pathname.endsWith("/article.html");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {isEpisodeRoute ? <EpisodePage /> : <Homepage />}
+    {isEpisodeRoute ? <EpisodePage /> : isArticleRoute ? <ArticleTemplate /> : <Homepage />}
   </StrictMode>,
 );
